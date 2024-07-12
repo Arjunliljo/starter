432d6b221b2a374d1fe22d66fc9aa1db63fe5704
Arjun cv <arjun7180@gmail.com>
2024-07-12 15:14:00 +0530

    added helmet http security feature

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const globalErrorHandler = require('./Utilities/errorController');
 // For limiting number of request from same IP address good security middleware
 const rateLimit = require('express-rate-limit');
 
+// For secure HTTP
+const helmet = require('helmet');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -13,12 +16,19 @@ const app = express();
 
 //GLOBAL MIDDLEWARES
 
-app.use(express.json());
+// Set security http headers
+app.use(helmet());
 
+// body parser : reading body from req.body
+// larger than 10kb file should not accepted by this
+app.use(express.json({ limit: '10kb' }));
+
+// development logging
 if (process.env.NODE_ENV === 'devlopment') {
   app.use(morgan('dev'));
 }
 
+// Test middle ware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
@@ -31,6 +41,7 @@ const limiter = rateLimit({
   message: 'Too many request from an IP, please try again later...',
 });
 
+// Limiting requests from a single IP
 app.use('/api', limiter);
 
 app.use('/api/v1/tours', tourRouter);
