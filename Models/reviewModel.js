a2d74bd548c5e50a8faac8364628223fb9eca59f
Arjun cv <arjun7180@gmail.com>
2024-07-15 23:43:32 +0530

    Created Review Shema

---
+++ Models/reviewModel.js
@@ -0,0 +1,38 @@
+//raiting createdAt, ref to tour, review, ref to user
+
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    require: [true, 'Review Must contains words..'],
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  rating: {
+    type: Number,
+    min: [1, 'rating should atleast 1'],
+    max: [2, 'rating should maximum 5'],
+    required: [true, 'Review Must have a rating..'],
+  },
+
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Review should belongs to a Tour'],
+  },
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review should belongs to a User'],
+  },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
